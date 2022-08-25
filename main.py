e4821d7f1936713d9ae692ec2e1e42636582779e
Christopher Marsh-Bourdon <christopher@boggybumblebee.com>
2022-08-25 12:44:44 +0200

    Created SVG conversion to BMP (via PNG) web service

--- main.py
+++ main.py
@@ -1,32 +1,59 @@
-import cairosvg
-from PIL import Image
-from typing import Union
+# SPDX-FileCopyrightText: 2022 Christopher Marsh-Bourdon (boggybumblebee)
+#
+# SPDX-License-Identifier: MIT
 
-from fastapi import FastAPI
+"""
+DEVELOPMENT HEALTH DIGITAL SIGNAGE for Adafruit Matrix Portal: displays stats about GitHub/SonarQube projects. 
+Requires WiFi internet access.
+
+Written by Christopher Marsh-Bourdon (boggybumblebee).
+MIT license, all text above must be included in any redistribution.
+
+BDF fonts from the X.Org project.
+"""
+
+import cairosvg
+from fastapi import FastAPI, HTTPException, Response
+from io import BytesIO
+from PIL import Image
+import urllib.request
+import urllib.error
 
 app = FastAPI()
 
+@app.get(
+    "/badges",
+    responses = {
+        200: {
+            "content": {"image/bmp": {}}
+        }
+    },
+    response_class=Response,
+)
+def read_badges(project: str, metric: str):
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+    # http://127.0.0.1:8000/badges?project=devhealth-digital-signage&metric=alert_status
+    # http://127.0.0.1:8000/badges?project=XYZ&metric=XYZ
 
+    url = "https://sonarcloud.io/api/project_badges/measure?project={0}&metric={1}".format(project, metric)
 
-@app.get("/badges")
-def read_root():
+    try:
+        # Import SVG in as a PNG (Stream)
+        png = cairosvg.svg2png(url=url)
+        img = Image.open(BytesIO(png))
 
-    cairosvg.svg2png(url="https://sonarcloud.io/api/project_badges/measure?project=devhealth-digital-signage&metric=sqale_rating", 
-        write_to="images/sqale_rating.png")
+        # Remove the Alpha from the PNG, as Bitmaps do not have/understand them
+        r, g, b, _ = img.split()
+        img = Image.merge("RGB", (r, g, b))
 
-    file_in = "images/sqale_rating.png"
-    img = Image.open(file_in)
+        # Return the BMP
+        return Response(content=image_to_byte_array(img), media_type="image/bmp")
 
-    r, g, b, _ = img.split()
-    img = Image.merge("RGB", (r, g, b))
-    file_out = "images/sqale_rating.bmp"
-    img.save(file_out)
-    return {"Saved": "File"}
+    except urllib.error.HTTPError as err:
+        raise HTTPException(status_code=404, detail="Badge not found")
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def image_to_byte_array(image: Image) -> bytes:
+    imgByteArr = BytesIO()
+    image.save(imgByteArr, format="bmp")
+    imgByteArr = imgByteArr.getvalue()
+    return imgByteArr
